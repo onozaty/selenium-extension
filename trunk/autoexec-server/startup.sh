e439ee0be2ae4869d8ba306b9ccc7cb0e6b8bc0a
onozaty <onozaty@d60d8bd2-d53f-0410-bb95-a9fee38a7ebe>
2008-10-25 18:21:46 +0000

    released Selenium AES version 0.4 changed startup/shutdown script.

--- trunk/autoexec-server/startup.sh
+++ trunk/autoexec-server/startup.sh
@@ -3,6 +3,6 @@
 # Selenium Auto Exec Server(AES) Startup.
 # ---------------------------------------------------
 
-SELENIUM_AES_CLASSPATH='selenium-autoexec-server-0.2.jar:lib/activation-1.1.jar:lib/commons-collections-3.2.jar:lib/commons-lang-2.3.jar:lib/mail-1.4.jar:lib/selenium-ext-htmlsuite-0.3.jar:lib/selenium-server-0.9.2.jar:lib/svnkit-1.1.4.jar:lib/velocity-1.5.jar'
+. ./set_classpath
 
 java -classpath $SELENIUM_AES_CLASSPATH com.enjoyxstudy.selenium.autoexec.AutoExecServer
