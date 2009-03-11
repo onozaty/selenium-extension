6a0edb717825ee59922c4cbd5d1adb9aad9d00cc
onozaty <onozaty@d60d8bd2-d53f-0410-bb95-a9fee38a7ebe>
2009-03-11 16:48:33 +0000

    added verify capture.

--- trunk/user-extension/error_capture/user-extensions.js
+++ trunk/user-extension/error_capture/user-extensions.js
@@ -4,6 +4,7 @@ if (!SeleniumExt) {
 
 SeleniumExt.ErrorCapture = {
   outputDir : "",
+  isCaptureEachVerify : false,
 
   getCommandUrl: function() {
     if (!SeleniumExt.ErrorCapture.commandURL) {
@@ -35,32 +36,66 @@ SeleniumExt.ErrorCapture = {
     return SeleniumExt.ErrorCapture.send("captureScreenshot", {"1": fileName});
   },
 
+  capture: function(testCase) {
+    var caseName = SeleniumExt.ErrorCapture.extractFileName(testCase.testDocument);
+    var suiteName = SeleniumExt.ErrorCapture.extractFileName(testCase.htmlTestSuiteRow.htmlTestSuite.suiteDocument);
+    var fileName = (SeleniumExt.ErrorCapture.outputDir || ".") + "/" + suiteName + "-" + caseName + "-" + (new Date()).getTime() + ".png";
+    var result = SeleniumExt.ErrorCapture.captureScreenshot(fileName);
+    if (result == "OK") {
+      LOG.info("capture error window: OK " + fileName);
+    } else {
+      LOG.warn("capture error window: " + result);
+    }
+  },
+
   extractFileName: function(doc) {
     var path = doc.location.pathname;
     return path.substring(path.lastIndexOf("/") + 1, path.indexOf("."));
-  },
-
-  testComplete: function() {
-    if (htmlTestRunner.testFailed) {
-      var caseName = SeleniumExt.ErrorCapture.extractFileName(this.htmlTestCase.testDocument);
-      var suiteName = SeleniumExt.ErrorCapture.extractFileName(this.htmlTestCase.htmlTestSuiteRow.htmlTestSuite.suiteDocument);
-      var fileName = (SeleniumExt.ErrorCapture.outputDir || ".") + "/" + suiteName + "-" + caseName + "-" + (new Date()).getTime() + ".png";
-      var result = SeleniumExt.ErrorCapture.captureScreenshot(fileName);
-      if (result == "OK") {
-        LOG.info("capture error window: OK " + fileName);
-      } else {
-        LOG.warn("capture error window: " + result);
-      }
-    }
   }
 }
 
+HtmlRunnerTestLoop.prototype.$commandComplete = HtmlRunnerTestLoop.prototype.commandComplete;
+HtmlRunnerTestLoop.prototype.commandComplete = function(result) {
+  var commandObj = this.currentRow.getCommand();
+  var command = "";
+  if (commandObj) {
+    command = commandObj.command;
+  }
 
-HtmlRunnerTestLoop.prototype._testComplete = HtmlRunnerTestLoop.prototype.testComplete;
+  if (result.failed ||
+      SeleniumExt.ErrorCapture.isCaptureEachVerify && command.indexOf("verify") >= 0) {
+    SeleniumExt.ErrorCapture.isNextTimeCapture = true;
+  }
+
+  this.$commandComplete(result);
+}
+
+HtmlRunnerTestLoop.prototype.$commandError = HtmlRunnerTestLoop.prototype.commandError;
+HtmlRunnerTestLoop.prototype.commandError = function(errorMessage) {
+  SeleniumExt.ErrorCapture.isNextTimeCapture = true;
+
+  this.$commandError(errorMessage);
+}
+
+
+HtmlRunnerTestLoop.prototype.$continueTestAtCurrentCommand = HtmlRunnerTestLoop.prototype.continueTestAtCurrentCommand;
+HtmlRunnerTestLoop.prototype.continueTestAtCurrentCommand = function() {
+  if (SeleniumExt.ErrorCapture.isNextTimeCapture) {
+    SeleniumExt.ErrorCapture.capture(this.htmlTestCase);
+    SeleniumExt.ErrorCapture.isNextTimeCapture = false;
+  }
+  this.$continueTestAtCurrentCommand();
+}
+
+HtmlRunnerTestLoop.prototype.$testComplete = HtmlRunnerTestLoop.prototype.testComplete;
 HtmlRunnerTestLoop.prototype.testComplete = function() {
   var self = this;
   window.setTimeout(function() {
-    SeleniumExt.ErrorCapture.testComplete.apply(self, arguments);
-    HtmlRunnerTestLoop.prototype._testComplete.apply(self, arguments);
+    if (SeleniumExt.ErrorCapture.isNextTimeCapture) {
+      SeleniumExt.ErrorCapture.capture(self.htmlTestCase);
+      SeleniumExt.ErrorCapture.isNextTimeCapture = false;
+    }
   }, 1);
+  this.$testComplete();
 }
+
