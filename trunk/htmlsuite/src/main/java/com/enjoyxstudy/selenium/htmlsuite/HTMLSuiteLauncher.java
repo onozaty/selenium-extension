a85458fbb546cf71f56dc3a87d935bae3149d033
onozaty <onozaty@d60d8bd2-d53f-0410-bb95-a9fee38a7ebe>
2009-03-21 15:23:14 +0000

    updated selenium-rc version 1.0b1 to 1.0b2.

--- trunk/htmlsuite/src/main/java/com/enjoyxstudy/selenium/htmlsuite/HTMLSuiteLauncher.java
+++ trunk/htmlsuite/src/main/java/com/enjoyxstudy/selenium/htmlsuite/HTMLSuiteLauncher.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2007, 2008 onozaty (http://www.enjoyxstudy.com)
+ * Copyright (c) 2007 - 2009 onozaty (http://www.enjoyxstudy.com)
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -17,42 +17,36 @@
 package com.enjoyxstudy.selenium.htmlsuite;
 
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 
 import org.apache.commons.logging.Log;
 import org.mortbay.log.LogFactory;
-import org.openqa.selenium.server.SeleniumCommandTimedOutException;
 import org.openqa.selenium.server.SeleniumServer;
-import org.openqa.selenium.server.BrowserSessionFactory.BrowserSessionInfo;
-import org.openqa.selenium.server.browserlaunchers.AsyncExecute;
-import org.openqa.selenium.server.browserlaunchers.BrowserLauncher;
-import org.openqa.selenium.server.browserlaunchers.BrowserLauncherFactory;
-import org.openqa.selenium.server.browserlaunchers.LauncherUtils;
-import org.openqa.selenium.server.htmlrunner.HTMLResultsListener;
+import org.openqa.selenium.server.htmlrunner.HTMLLauncher;
 import org.openqa.selenium.server.htmlrunner.HTMLTestResults;
 
 /**
- * base org.openqa.selenium.server.htmlrunner.HTMLLauncher
- * 
  * @author onozaty
  */
-public class HTMLSuiteLauncher implements HTMLResultsListener {
+public class HTMLSuiteLauncher extends HTMLLauncher {
+
+    /** result passed string */
+    private static final String RESULT_PASSED = "PASSED";
+
+    /** create temp result file use prefix */
+    private static final String TEMP_RESULT_FILE_PREFIX = "selenium";
 
     /** logger */
     private static Log log = LogFactory.getLog(HTMLSuiteLauncher.class);
-
-    /** SeleniumServer */
-    private SeleniumServer server;
 
     /** results */
     private HTMLTestResults results;
 
     /**
-     * @param server
+     * @param remoteControl
      */
-    public HTMLSuiteLauncher(SeleniumServer server) {
-        this.server = server;
+    public HTMLSuiteLauncher(SeleniumServer remoteControl) {
+        super(remoteControl);
     }
 
     /**
@@ -60,18 +54,18 @@ public class HTMLSuiteLauncher implements HTMLResultsListener {
      * 
      * @param browser - the browserString ("*firefox", "*iexplore" or an executable path)
      * @param browserURL - the start URL for the browser
-     * @param suiteFilePath - a file containing the HTML suite to run
-     * @param resultFilePath - The file to which we'll output the HTML results
+     * @param suiteFile - a file containing the HTML suite to run
      * @param timeoutInSeconds - the amount of time (in seconds) to wait for the browser to finish
+     * @param multiWindow 
      * @return result
      * @throws IOException if we can't read the write file
      */
-    public boolean runHTMLSuite(String browser, String browserURL,
-            String suiteFilePath, String resultFilePath, int timeoutInSeconds)
+    public boolean runHTMLSuiteResult(String browser, String browserURL,
+            File suiteFile, int timeoutInSeconds, boolean multiWindow)
             throws IOException {
 
-        return runHTMLSuite(browser, browserURL, new File(suiteFilePath),
-                new File(resultFilePath), timeoutInSeconds);
+        return runHTMLSuiteResult(browser, browserURL, suiteFile, null,
+                timeoutInSeconds, multiWindow);
     }
 
     /**
@@ -82,142 +76,43 @@ public class HTMLSuiteLauncher implements HTMLResultsListener {
      * @param suiteFile - a file containing the HTML suite to run
      * @param resultFile - The file to which we'll output the HTML results
      * @param timeoutInSeconds - the amount of time (in seconds) to wait for the browser to finish
+     * @param multiWindow 
      * @return result
      * @throws IOException if we can't read the write file
      */
-    public boolean runHTMLSuite(String browser, String browserURL,
-            File suiteFile, File resultFile, int timeoutInSeconds)
-            throws IOException {
+    public boolean runHTMLSuiteResult(String browser, String browserURL,
+            File suiteFile, File resultFile, int timeoutInSeconds,
+            boolean multiWindow) throws IOException {
 
-        resultFile.createNewFile();
-        if (!resultFile.canWrite()) {
-            throw new IOException("Can't write to outputFile: "
-                    + resultFile.getAbsolutePath());
-        }
+        log.info("HTML Suite Start. suite[" + suiteFile + "] browser["
+                + browser + "]");
 
-        boolean result = runHTMLSuite(browser, browserURL, suiteFile,
-                timeoutInSeconds);
+        processResults(null); // init result
 
-        FileWriter fileWriter = new FileWriter(resultFile);
-        try {
-            results.write(fileWriter);
-        } finally {
-            fileWriter.close();
-        }
-
-        return result;
-    }
-
-    /**
-     * Launches a single HTML Selenium test suite.
-     * 
-     * @param browser - the browserString ("*firefox", "*iexplore" or an executable path)
-     * @param browserURL - the start URL for the browser
-     * @param suiteFilePath - a file containing the HTML suite to run
-     * @param timeoutInSeconds - the amount of time (in seconds) to wait for the browser to finish
-     * @return result
-     * @throws IOException if we can't read the write file
-     */
-    public boolean runHTMLSuite(String browser, String browserURL,
-            String suiteFilePath, int timeoutInSeconds) throws IOException {
-
-        return runHTMLSuite(browser, browserURL, new File(suiteFilePath),
-                timeoutInSeconds);
-    }
-
-    /**
-     * Launches a single HTML Selenium test suite.
-     * 
-     * @param browser - the browserString ("*firefox", "*iexplore" or an executable path)
-     * @param browserURL - the start URL for the browser
-     * @param suiteFile - a file containing the HTML suite to run
-     * @param timeoutInSeconds - the amount of time (in seconds) to wait for the browser to finish
-     * @return result
-     * @throws IOException if we can't read the write file
-     */
-    public boolean runHTMLSuite(String browser, String browserURL,
-            File suiteFile, int timeoutInSeconds) throws IOException {
-
-        if (browser == null)
-            throw new IllegalArgumentException("browser may not be null");
-        if (!suiteFile.exists()) {
-            throw new IOException("Can't find HTML Suite file:"
-                    + suiteFile.getAbsolutePath());
-        }
-        if (!suiteFile.canRead()) {
-            throw new IOException("Can't read HTML Suite file: "
-                    + suiteFile.getAbsolutePath());
-        }
         if (!suiteFile.isAbsolute()) {
             suiteFile = suiteFile.getAbsoluteFile();
         }
 
-        server.addNewStaticContent(suiteFile.getParentFile());
-
-        // DGF this is a hack, but I can't find a better place to put it
-        String suiteURL;
-        if (browser.startsWith("*chrome") || browser.startsWith("*iehta")) {
-            suiteURL = "http://localhost:"
-                    + SeleniumServer.getPortDriversShouldContact()
-                    + "/selenium-server/tests/" + suiteFile.getName();
-        } else {
-            suiteURL = LauncherUtils.stripStartURL(browserURL)
-                    + "/selenium-server/tests/" + suiteFile.getName();
-        }
-        return run(browser, browserURL, suiteURL, timeoutInSeconds);
-    }
-
-    /**
-     * Launches a single HTML Selenium test suite.
-     * 
-     * @param browser - the browserString ("*firefox", "*iexplore" or an executable path)
-     * @param browserURL - the start URL for the browser
-     * @param suiteURL - the relative URL to the HTML suite
-     * @param timeoutInSeconds - the amount of time (in seconds) to wait for the browser to finish
-     * @return result
-     */
-    private boolean run(String browser, String browserURL, String suiteURL,
-            int timeoutInSeconds) {
-
-        String suiteName = suiteURL.substring(suiteURL.lastIndexOf('/') + 1);
-        log.info("HTML Suite Start. suite[" + suiteName + "] browser["
-                + browser + "]");
-
-        results = null;
-
-        long timeoutInMs = 1000l * timeoutInSeconds;
-        if (timeoutInMs < 0) {
-            log
-                    .warn("Looks like the timeout overflowed, so resetting it to the maximum.");
-            timeoutInMs = Long.MAX_VALUE;
-        }
-        server.handleHTMLRunnerResults(this);
-        BrowserLauncherFactory blf = new BrowserLauncherFactory();
-        String sessionId = Long.toString(System.currentTimeMillis() % 1000000);
-        BrowserLauncher launcher = blf.getBrowserLauncher(browser, sessionId);
-
-        BrowserSessionInfo sessionInfo = new BrowserSessionInfo(sessionId,
-                browser, browserURL, launcher, null);
-        server.registerBrowserSession(sessionInfo);
-
-        // JB: -- aren't these URLs in the wrong order according to declaration?
-        launcher.launchHTMLSuite(suiteURL, browserURL, server.isMultiWindow(),
-                "info");
-        long now = System.currentTimeMillis();
-        long end = now + timeoutInMs;
-        while (results == null && System.currentTimeMillis() < end) {
-            AsyncExecute.sleepTight(500);
-        }
-        launcher.close();
-        server.deregisterBrowserSession(sessionInfo);
-        if (results == null) {
-            throw new SeleniumCommandTimedOutException();
+        boolean isResultTemprary = false;
+        if (resultFile == null) {
+            resultFile = File.createTempFile(TEMP_RESULT_FILE_PREFIX, null);
+            isResultTemprary = true;
         }
 
-        boolean result = results.getResult().toUpperCase().equals("PASSED");
+        String resultString;
+        try {
+            resultString = runHTMLSuite(browser, browserURL, suiteFile,
+                    resultFile, timeoutInSeconds, multiWindow);
+        } finally {
+            if (isResultTemprary) {
+                resultFile.delete();
+            }
+        }
 
-        String endMessage = "HTML Suite End. result[" + results.getResult()
-                + "] suite[" + suiteName + "] browser[" + browser + "]";
+        boolean result = resultString.equals(RESULT_PASSED);
+
+        String endMessage = "HTML Suite End. result[" + resultString
+                + "] suite[" + suiteFile + "] browser[" + browser + "]";
 
         if (result) {
             log.info(endMessage);
@@ -231,7 +126,9 @@ public class HTMLSuiteLauncher implements HTMLResultsListener {
      * Accepts HTMLTestResults for later asynchronous handling 
      * @param resultsParm 
      */
+    @Override
     public void processResults(HTMLTestResults resultsParm) {
+        super.processResults(resultsParm);
         this.results = resultsParm;
     }
 
--- trunk/htmlsuite/src/test/java/com/enjoyxstudy/selenium/htmlsuite/MultiHTMLSuiteRunnerTest.java
+++ trunk/htmlsuite/src/test/java/com/enjoyxstudy/selenium/htmlsuite/MultiHTMLSuiteRunnerTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2007, 2008 onozaty (http://www.enjoyxstudy.com)
+ * Copyright (c) 2007 - 2009 onozaty (http://www.enjoyxstudy.com)
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -37,7 +37,7 @@ public class MultiHTMLSuiteRunnerTest extends TestCase {
         try {
             server.start();
             MultiHTMLSuiteRunner runner = new MultiHTMLSuiteRunner(server);
-            runner.addHTMLSuites("*firefox", "http://www.google.com/",
+            runner.addHTMLSuites("*firefoxproxy", "http://www.google.com/",
                     new String[] { "suite/pattern1/suite1.html" }, 30);
 
             assertTrue(runner.runHTMLSuites());
@@ -62,7 +62,7 @@ public class MultiHTMLSuiteRunnerTest extends TestCase {
         try {
             server.start();
             MultiHTMLSuiteRunner runner = new MultiHTMLSuiteRunner(server);
-            runner.addHTMLSuites("*firefox", "http://www.google.com/",
+            runner.addHTMLSuites("*firefoxproxy", "http://www.google.com/",
                     new String[] { "suite/pattern1/failed1.html",
                             "suite/pattern1/suite3.html" }, 30);
 
@@ -97,7 +97,7 @@ public class MultiHTMLSuiteRunnerTest extends TestCase {
         try {
             server.start();
             MultiHTMLSuiteRunner runner = new MultiHTMLSuiteRunner(server);
-            runner.addHTMLSuites("*firefox", "http://www.google.com/",
+            runner.addHTMLSuites("*firefoxproxy", "http://www.google.com/",
                     new String[] { "suite/pattern1/suite2.html",
                             "suite/pattern1/suite1.html" }, "result", 30);
 
@@ -133,29 +133,30 @@ public class MultiHTMLSuiteRunnerTest extends TestCase {
         try {
             server.start();
             MultiHTMLSuiteRunner runner = new MultiHTMLSuiteRunner(server);
-            runner.addHTMLSuites(new String[] { "*firefox", "*iexplore" },
-                    "http://www.google.com/", "suite/pattern1", 30);
+            runner.addHTMLSuites(new String[] { "*firefoxproxy",
+                    "*iexploreproxy" }, "http://www.google.com/",
+                    "suite/pattern1", 30);
 
             assertTrue(runner.runHTMLSuites());
             assertEquals(6, runner.getHtmlSuiteList().size());
 
             assertEquals("suite1.html", runner.getHtmlSuiteList().get(0)
                     .getSuiteFile().getName());
-            assertEquals("*firefox", runner.getHtmlSuiteList().get(0)
+            assertEquals("*firefoxproxy", runner.getHtmlSuiteList().get(0)
                     .getBrowser());
             assertEquals("passed", runner.getHtmlSuiteList().get(0)
                     .getTestResults().getResult());
 
             assertEquals("suite2.html", runner.getHtmlSuiteList().get(1)
                     .getSuiteFile().getName());
-            assertEquals("*firefox", runner.getHtmlSuiteList().get(1)
+            assertEquals("*firefoxproxy", runner.getHtmlSuiteList().get(1)
                     .getBrowser());
             assertEquals("passed", runner.getHtmlSuiteList().get(1)
                     .getTestResults().getResult());
 
             assertEquals("suite3.html", runner.getHtmlSuiteList().get(2)
                     .getSuiteFile().getName());
-            assertEquals("*firefox", runner.getHtmlSuiteList().get(2)
+            assertEquals("*firefoxproxy", runner.getHtmlSuiteList().get(2)
                     .getBrowser());
             assertEquals("passed", runner.getHtmlSuiteList().get(2)
                     .getTestResults().getResult());
@@ -163,21 +164,21 @@ public class MultiHTMLSuiteRunnerTest extends TestCase {
 
             assertEquals("suite1.html", runner.getHtmlSuiteList().get(3)
                     .getSuiteFile().getName());
-            assertEquals("*iexplore", runner.getHtmlSuiteList().get(3)
+            assertEquals("*iexploreproxy", runner.getHtmlSuiteList().get(3)
                     .getBrowser());
             assertEquals("passed", runner.getHtmlSuiteList().get(3)
                     .getTestResults().getResult());
 
             assertEquals("suite2.html", runner.getHtmlSuiteList().get(4)
                     .getSuiteFile().getName());
-            assertEquals("*iexplore", runner.getHtmlSuiteList().get(4)
+            assertEquals("*iexploreproxy", runner.getHtmlSuiteList().get(4)
                     .getBrowser());
             assertEquals("passed", runner.getHtmlSuiteList().get(4)
                     .getTestResults().getResult());
 
             assertEquals("suite3.html", runner.getHtmlSuiteList().get(5)
                     .getSuiteFile().getName());
-            assertEquals("*iexplore", runner.getHtmlSuiteList().get(5)
+            assertEquals("*iexploreproxy", runner.getHtmlSuiteList().get(5)
                     .getBrowser());
             assertEquals("passed", runner.getHtmlSuiteList().get(5)
                     .getTestResults().getResult());
@@ -198,7 +199,7 @@ public class MultiHTMLSuiteRunnerTest extends TestCase {
         try {
             server.start();
             MultiHTMLSuiteRunner runner = new MultiHTMLSuiteRunner(server);
-            runner.addHTMLSuites(new String[] { "*iexplore", },
+            runner.addHTMLSuites(new String[] { "*iexploreproxy", },
                     "http://www.google.com/", "suite/pattern1/suite1.html", 30);
 
             assertTrue(runner.runHTMLSuites());
@@ -206,7 +207,7 @@ public class MultiHTMLSuiteRunnerTest extends TestCase {
 
             assertEquals("suite1.html", runner.getHtmlSuiteList().get(0)
                     .getSuiteFile().getName());
-            assertEquals("*iexplore", runner.getHtmlSuiteList().get(0)
+            assertEquals("*iexploreproxy", runner.getHtmlSuiteList().get(0)
                     .getBrowser());
             assertEquals("passed", runner.getHtmlSuiteList().get(0)
                     .getTestResults().getResult());
@@ -231,16 +232,16 @@ public class MultiHTMLSuiteRunnerTest extends TestCase {
         try {
             server.start();
             MultiHTMLSuiteRunner runner = new MultiHTMLSuiteRunner(server);
-            runner.addHTMLSuites(new String[] { "*iexplore", "*firefox" },
-                    "http://www.google.com/", "suite/pattern1/suite1.html",
-                    "result", 30);
+            runner.addHTMLSuites(new String[] { "*iexploreproxy",
+                    "*firefoxproxy" }, "http://www.google.com/",
+                    "suite/pattern1/suite1.html", "result", 30);
 
             assertTrue(runner.runHTMLSuites());
             assertEquals(2, runner.getHtmlSuiteList().size());
 
             assertEquals("suite1.html", runner.getHtmlSuiteList().get(0)
                     .getSuiteFile().getName());
-            assertEquals("*iexplore", runner.getHtmlSuiteList().get(0)
+            assertEquals("*iexploreproxy", runner.getHtmlSuiteList().get(0)
                     .getBrowser());
             assertEquals("passed", runner.getHtmlSuiteList().get(0)
                     .getTestResults().getResult());
@@ -248,7 +249,7 @@ public class MultiHTMLSuiteRunnerTest extends TestCase {
 
             assertEquals("suite1.html", runner.getHtmlSuiteList().get(1)
                     .getSuiteFile().getName());
-            assertEquals("*firefox", runner.getHtmlSuiteList().get(1)
+            assertEquals("*firefoxproxy", runner.getHtmlSuiteList().get(1)
                     .getBrowser());
             assertEquals("passed", runner.getHtmlSuiteList().get(1)
                     .getTestResults().getResult());
@@ -272,7 +273,7 @@ public class MultiHTMLSuiteRunnerTest extends TestCase {
         try {
             server.start();
             MultiHTMLSuiteRunner runner = new MultiHTMLSuiteRunner(server);
-            runner.addHTMLSuiteDir("*firefox", "http://www.google.com/",
+            runner.addHTMLSuiteDir("*firefoxproxy", "http://www.google.com/",
                     "suite/pattern1", 30);
 
             assertTrue(runner.runHTMLSuites());
@@ -316,7 +317,7 @@ public class MultiHTMLSuiteRunnerTest extends TestCase {
         try {
             server.start();
             MultiHTMLSuiteRunner runner = new MultiHTMLSuiteRunner(server);
-            runner.addHTMLSuiteDir("*firefox", "http://www.google.com/",
+            runner.addHTMLSuiteDir("*firefoxproxy", "http://www.google.com/",
                     "suite/pattern1", "result", 30);
 
             assertTrue(runner.runHTMLSuites());
@@ -362,23 +363,23 @@ public class MultiHTMLSuiteRunnerTest extends TestCase {
         try {
             server.start();
             MultiHTMLSuiteRunner runner = new MultiHTMLSuiteRunner(server);
-            runner.addHTMLSuiteGenerate(
-                    new String[] { "*firefox", "*iexplore" },
-                    "http://www.google.com/", "suite/pattern1", 60);
+            runner.addHTMLSuiteGenerate(new String[] { "*firefoxproxy",
+                    "*iexploreproxy" }, "http://www.google.com/",
+                    "suite/pattern1", 60);
 
             assertFalse(runner.runHTMLSuites());
             assertEquals(2, runner.getHtmlSuiteList().size());
 
             assertEquals("generatedTestSuite.html", runner.getHtmlSuiteList()
                     .get(0).getSuiteFile().getName());
-            assertEquals("*firefox", runner.getHtmlSuiteList().get(0)
+            assertEquals("*firefoxproxy", runner.getHtmlSuiteList().get(0)
                     .getBrowser());
             assertEquals("failed", runner.getHtmlSuiteList().get(0)
                     .getTestResults().getResult());
 
             assertEquals("generatedTestSuite.html", runner.getHtmlSuiteList()
                     .get(1).getSuiteFile().getName());
-            assertEquals("*iexplore", runner.getHtmlSuiteList().get(1)
+            assertEquals("*iexploreproxy", runner.getHtmlSuiteList().get(1)
                     .getBrowser());
             assertEquals("failed", runner.getHtmlSuiteList().get(1)
                     .getTestResults().getResult());
@@ -402,7 +403,7 @@ public class MultiHTMLSuiteRunnerTest extends TestCase {
         try {
             server.start();
             MultiHTMLSuiteRunner runner = new MultiHTMLSuiteRunner(server);
-            runner.addHTMLSuiteGenerate(new String[] { "*iexplore" },
+            runner.addHTMLSuiteGenerate(new String[] { "*iexploreproxy" },
                     "http://www.google.com/", "suite/pattern1", "result", 60);
 
             assertFalse(runner.runHTMLSuites());
@@ -410,7 +411,7 @@ public class MultiHTMLSuiteRunnerTest extends TestCase {
 
             assertEquals("generatedTestSuite.html", runner.getHtmlSuiteList()
                     .get(0).getSuiteFile().getName());
-            assertEquals("*iexplore", runner.getHtmlSuiteList().get(0)
+            assertEquals("*iexploreproxy", runner.getHtmlSuiteList().get(0)
                     .getBrowser());
             assertEquals("failed", runner.getHtmlSuiteList().get(0)
                     .getTestResults().getResult());
@@ -433,15 +434,16 @@ public class MultiHTMLSuiteRunnerTest extends TestCase {
         try {
             server.start();
             MultiHTMLSuiteRunner runner = new MultiHTMLSuiteRunner(server);
-            runner.addHTMLSuiteGenerate(new String[] { "*iexplore", "*firefox" },
-                    "http://www.google.com/", "suite", "result", 60);
+            runner.addHTMLSuiteGenerate(new String[] { "*iexploreproxy",
+                    "*firefoxproxy" }, "http://www.google.com/", "suite",
+                    "result", 60);
 
             assertFalse(runner.runHTMLSuites());
             assertEquals(4, runner.getHtmlSuiteList().size());
 
-            assertEquals("pattern1.html", runner.getHtmlSuiteList()
-                    .get(0).getSuiteFile().getName());
-            assertEquals("*iexplore", runner.getHtmlSuiteList().get(0)
+            assertEquals("pattern1.html", runner.getHtmlSuiteList().get(0)
+                    .getSuiteFile().getName());
+            assertEquals("*iexploreproxy", runner.getHtmlSuiteList().get(0)
                     .getBrowser());
             assertEquals("failed", runner.getHtmlSuiteList().get(0)
                     .getTestResults().getResult());
